b7d685b6f265fa0eadbbcf447c51277abdedfaa4
liyizheng0513 <1658290151@qq.com>
2017-04-12 15:46:08 +0800

    alpha formula

--- alpha.py
+++ alpha.py
@@ -51,12 +51,12 @@ def factor_handle(factor):
     return factor
 
 
-def group_backtest(factor, volume, close, group_num, quantile, fee):
+def group_backtest(factor, volume, close, group_num, quantile, fee, period):
     pct_chg = close.pct_change()
     stockpool = pd.Series(np.zeros(factor.shape[1]), index=factor.columns)
     cash = 1.0
     net_value = []
-    for i in range(1, factor.shape[0]):
+    for i in range(1, factor.shape[0], period):
         date = factor.index[i]
         factor_today = factor.ix[factor.index[i - 1]].sort_values().dropna()
         close_today = close.ix[date]
@@ -64,7 +64,7 @@ def group_backtest(factor, volume, close, group_num, quantile, fee):
         vol_today = volume.ix[date]
         inteval_len = factor_today.shape[0] / group_num
         tobuy = factor_today[
-                (quantile - 1) * inteval_len:quantile * inteval_len].index
+            (quantile - 1) * inteval_len:quantile * inteval_len].index
         tosell = stockpool[stockpool > 0].index
         first_sell = list(set(tosell) - set(tobuy))
         for stock in first_sell:
@@ -73,7 +73,8 @@ def group_backtest(factor, volume, close, group_num, quantile, fee):
                 stockpool[stock] = 0.0
         last_buy = list(set(tobuy) - set(tosell))
         buy_num = len(last_buy)
-        per_money = cash / (buy_num + 0.0)
+        if buy_num > 0:
+            per_money = cash / (buy_num + 0.0)
         for stock in last_buy:
             if pct_chg_today[stock] < 0.99 and vol_today[stock] > 0:
                 stockpool[stock] += per_money / close_today[stock] * (1 - fee)
@@ -81,14 +82,21 @@ def group_backtest(factor, volume, close, group_num, quantile, fee):
 
         pool = stockpool[stockpool > 0]
         net_value.append((pool * close_today[pool.index]).sum() + cash)
-    return pd.Series(net_value, index=factor.index[1:])
+    return pd.Series(
+        net_value,
+        index=factor.index[
+            range(
+                1,
+                factor.shape[0],
+                period)])
 
 
 def group_result(pctchange, dic, period):
     net_value = dict()
     for s in dic.keys():
         group = dic[s]
-        zz = pd.Series([pctchange.iloc[i][dic[s][i - period]].mean() for i in range(period, len(pctchange))]) + 1
+        zz = pd.Series([pctchange.iloc[i][dic[s][i - period]].mean()
+                        for i in range(period, len(pctchange))]) + 1
         net_value[s] = zz.cumprod()
     res = pd.DataFrame(net_value)
     res.index = pctchange.index[period:]
@@ -104,7 +112,12 @@ def generate_group(factor, group_num):
         temp = temp.dropna().sort_values()
         interval = len(temp) / group_num
         for quantile in range(1, group_num + 1):
-            dic[quantile].append(temp[(quantile - 1) * interval:quantile * interval].index)
+            dic[quantile].append(
+                temp[
+                    (quantile -
+                     1) *
+                    interval:quantile *
+                    interval].index)
     return dic
 
 
@@ -126,7 +139,8 @@ def quick_test(factor, pctChange, group_num, period):
 
 
 def hedge_curve(net_value, benchmark_code):
-    benchmark = w.wsd(benchmark_code, "pct_chg", net_value.index[0], net_value.index[-1], "PriceAdj=B")
+    benchmark = w.wsd(benchmark_code, "pct_chg", net_value.index[
+                      0], net_value.index[-1], "PriceAdj=B")
     benchmark = pd.Series(benchmark.Data[0], index=net_value.index) / 100.0
     net_value_pct = net_value.pct_change()
     hedge_res = (net_value_pct.apply(lambda x: x - benchmark) + 1).cumprod()
@@ -135,7 +149,7 @@ def hedge_curve(net_value, benchmark_code):
 
 def returns_sta(net_value):
     grouped = net_value.groupby(lambda x: x.split('-')[0])
-    return grouped.apply(lambda x: x.iloc[-1]/x.iloc[0]-1)
+    return grouped.apply(lambda x: x.iloc[-1] / x.iloc[0] - 1)
 
 
 def cap_neutral(factor, mkt_value):
@@ -160,14 +174,18 @@ def t_value(factor, pctchange, period):
     pctchange_copy = pctchange.ix[new_factor.index]
     for i in range(new_factor.shape[0] - period):
         factor_value = new_factor.iloc[i].dropna()
-        pct_chg = pctchange_copy.iloc[i + period].ix[factor_value.index].dropna()
+        pct_chg = pctchange_copy.iloc[
+            i +
+            period].ix[
+            factor_value.index].dropna()
         factor_value = factor_value[pct_chg.index]
         results = sm.OLS(pct_chg, factor_value).fit()
         tvalue = results.tvalues[0]
         rsquare = results.rsquared
         tvalues.append(tvalue)
         rsquares.append(rsquare)
-    return pd.DataFrame({'tvalue': tvalues, 'rsquare': rsquares}, index=new_factor.index[:-period])
+    return pd.DataFrame({'tvalue': tvalues, 'rsquare': rsquares},
+                        index=new_factor.index[:-period])
 
 
 def tvalue_sta(tvalues):
@@ -180,7 +198,7 @@ def tvalue_sta(tvalues):
 def quantile_mkt_values(signal_df, mkt_df):
     n_quantile = 10
     # 统计十分位数
-    cols_mean = [i+1 for i in range(n_quantile)]
+    cols_mean = [i + 1 for i in range(n_quantile)]
     cols = cols_mean
 
     mkt_value_means = pd.DataFrame(index=signal_df.index, columns=cols)
@@ -193,14 +211,16 @@ def quantile_mkt_values(signal_df, mkt_df):
 
         tmp_factor = signal_df.ix[dt].dropna()
         tmp_mkt_value = mkt_df.ix[dt].dropna()
-        tmp_mkt_value = tmp_mkt_value.rank()/len(tmp_mkt_value)
+        tmp_mkt_value = tmp_mkt_value.rank() / len(tmp_mkt_value)
 
-        pct_quantiles = 1.0/n_quantile
+        pct_quantiles = 1.0 / n_quantile
         for i in range(n_quantile):
             down = tmp_factor.quantile(
-                pct_quantiles*i)
-            up = tmp_factor.quantile(pct_quantiles*(i+1))
-            i_quantile_index = tmp_factor[(tmp_factor <= up) & (tmp_factor >= down)].index
+                pct_quantiles * i)
+            up = tmp_factor.quantile(pct_quantiles * (i + 1))
+            i_quantile_index = tmp_factor[
+                (tmp_factor <= up) & (
+                    tmp_factor >= down)].index
             mean_tmp = tmp_mkt_value[i_quantile_index].mean()
             qt_mean_results.append(mean_tmp)
         mkt_value_means.ix[dt] = qt_mean_results
